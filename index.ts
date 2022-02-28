bee13830f09d3da29d08b84d87274b5e5926d5a0
Daniel Bradley <daniel@pulumi.com>
2022-02-28 15:11:25 +0000

    End state

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as operationalinsights from "@pulumi/azure-native/operationalinsights";
-import * as storage from "@pulumi/azure-native/storage";
-import * as app from "@pulumi/azure-native/app/v20220101preview";
+import * as app from "@pulumi/azure-native/app";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup");
@@ -19,100 +18,7 @@ const sharedKeys = operationalinsights.getSharedKeysOutput({
   workspaceName: workspace.name,
 });
 
-///////// Original
-
-// const kubeEnvironment = new web.KubeEnvironment("kubeEnvironment", {
-//   resourceGroupName: resourceGroup.name,
-//   environmentType: "Managed",
-//   appLogsConfiguration: {
-//     destination: "log-analytics",
-//     logAnalyticsConfiguration: {
-//       customerId: workspace.customerId,
-//       sharedKey: sharedKeys.primarySharedKey?.apply((k) => k!),
-//     },
-//   },
-// });
-
-// const containerAppSimple = new web.ContainerApp("containerAppSimple", {
-//   resourceGroupName: resourceGroup.name,
-//   name: "container-app-simple",
-//   kubeEnvironmentId: kubeEnvironment.id,
-//   configuration: {
-//     ingress: {
-//       external: true,
-//       targetPort: 80,
-//     },
-//   },
-//   template: {
-//     containers: [
-//       {
-//         name: "web",
-//         image: "nginx",
-//         resources: { cpu: 0.25, memory: "0.5Gi" },
-//       },
-//     ],
-//   },
-// });
-
-/////////// Step 1
-
 const kubeEnvironment = new app.ManagedEnvironment(
-  "kubeEnvironment",
-  {
-    resourceGroupName: resourceGroup.name,
-    // environmentType: "Managed", // Removed
-    appLogsConfiguration: {
-      destination: "log-analytics",
-      logAnalyticsConfiguration: {
-        customerId: workspace.customerId,
-        sharedKey: sharedKeys.primarySharedKey?.apply((k) => k!),
-      },
-    },
-  },
-  {
-    aliases: [
-      "urn:pulumi:dev::azure-container-migration::azure-native:web/v20210301:KubeEnvironment::kubeEnvironment",
-    ],
-    ignoreChanges: ["environmentType"],
-    retainOnDelete: true,
-  }
-);
-
-const containerAppSimple = new app.ContainerApp(
-  "containerAppSimple",
-  {
-    resourceGroupName: resourceGroup.name,
-    name: "container-app-simple",
-    // kubeEnvironmentId: kubeEnvironment.id,
-    managedEnvironmentId: kubeEnvironment.id,
-    configuration: {
-      ingress: {
-        external: true,
-        targetPort: 80,
-      },
-    },
-    template: {
-      containers: [
-        {
-          name: "web",
-          image: "nginx",
-          resources: { cpu: 0.25, memory: "0.5Gi" },
-        },
-      ],
-    },
-  },
-  {
-    aliases: [
-      "urn:pulumi:dev::azure-container-migration::azure-native:web/v20210301:ContainerApp::containerAppSimple",
-    ],
-    ignoreChanges: ["kubeEnvironmentId", "managedEnvironmentId"],
-    retainOnDelete: true,
-  }
-);
-
-////////// Step 2
-
-const kubeEnvironmentImported = new app.ManagedEnvironment(
   "kubeEnvironmentImported",
   {
     resourceGroupName: resourceGroup.name,
@@ -132,12 +38,12 @@ const kubeEnvironmentImported = new app.ManagedEnvironment(
   }
 );
 
-const containerAppSimpleImport = new app.ContainerApp(
+const containerAppSimple = new app.ContainerApp(
   "containerAppSimpleImported",
   {
     resourceGroupName: resourceGroup.name,
     name: "container-app-simple",
-    managedEnvironmentId: kubeEnvironmentImported.id,
+    managedEnvironmentId: kubeEnvironment.id,
     configuration: {
       ingress: {
         external: true,
